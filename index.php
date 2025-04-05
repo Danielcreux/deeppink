86da3962e40eed716624d2c9d386af78328f5438
Danielcreux <jd2000_fs@hotmail.com>
2025-04-05 21:13:04 +0200

    primer commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    $contenido = file_get_contents("https://elpais.com/");
+
+
+    libxml_use_internal_errors(true); // Avoid warnings on malformed HTML
+
+    $doc = new DOMDocument();
+    $doc->loadHTML($contenido);
+
+    $titles = $doc->getElementsByTagName('title');
+    
+    foreach($titles as $valor){
+        echo $valor->textContent;
+    }
+    
+    $enlaces = $doc->getElementsByTagName('a');
+
+    foreach($enlaces as $valor){
+        echo $valor->textContent;
+    }
+?>
